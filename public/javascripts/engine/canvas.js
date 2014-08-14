d699514bae9767e6a2db94f7f59135193b4194c5
Andreas Hultgren <me@andreashultgren.se>
2014-08-14 20:12:11 +0200

    Canvas is always fullscreen

--- public/javascripts/engine/canvas.js
+++ public/javascripts/engine/canvas.js
@@ -1,12 +1,19 @@
 'use strict';
 
+var $ = require('jquery');
 var canvas = module.exports = document.querySelector('#canvas');
+var $canvas = $(canvas);
 
 // Set dimensions to css dimensions
-canvas.width = require('jquery')(canvas).width();
-canvas.height = require('jquery')(canvas).height();
+canvas.width = $canvas.width();
+canvas.height = $canvas.height();
 
 canvas.ctx = canvas.getContext('2d');
+
+$(window).on('resize', function () {
+  canvas.width = $canvas.width();
+  canvas.height = $canvas.height();
+});
 
 
 //## Dunny why the fuck this doesnt work...
