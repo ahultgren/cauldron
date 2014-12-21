7ecc47bd7bc40f364f08301fc581a1d5f506bbc8
Andreas Hultgren <me@andreashultgren.se>
2014-12-21 18:16:11 +0000

    Ditched rxjs for baconjs for routing

--- public/javascripts/ui/router.js
+++ public/javascripts/ui/router.js
@@ -1,25 +1,30 @@
 'use strict';
 
 var R = require('ramda');
-var Rx = require('rx/dist/rx.lite.min');
+var Bacon = require('baconjs');
 var Route = require('./route');
 var routes = [];
 
 var extractHash = R.compose(R.prop(1), R.split('#'));
-var matchRoute = R.nAry(1, R.compose(R.prop(0), R.flip(R.filter)(routes), R.flip(R.func('match'))));
+var matchPathOnEachRoute = R.nAry(1, R.compose(
+  R.prop(0),
+  R.flip(R.filter)(routes),
+  R.flip(R.func('match'))
+));
 var route = R.func('route');
 
-Rx.Observable.merge(
-  Rx.Observable.fromEvent(window, 'hashchange')
-    .map(R.prop('newURL')),
-  Rx.Observable.fromEvent(window, 'load')
-    .map(R.compose(R.path('location.href'), R.always(window)))
-  )
-  .map(extractHash)
-  .map(matchRoute)
+var hashchangeUrl = Bacon.fromEventTarget(window, 'hashchange')
+  .map(R.prop('newURL'));
+
+var domreadyUrl = Bacon.fromEventTarget(window, 'load')
+  .map(R.compose(R.path('location.href'), R.always(window)));
+
+var currentUrl = hashchangeUrl.merge(domreadyUrl);
+
+currentUrl.map(extractHash)
+  .map(matchPathOnEachRoute)
   .filter(Boolean)
-  .do(route)
-  .subscribe();
+  .onValue(route);
 
 exports.route = function (path, callback) {
   routes.push(Route.create(path, callback));
